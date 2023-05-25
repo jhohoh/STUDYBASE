31e772839952fa74b957f5ec05fc4db20dedca0d
DESKTOP-EI92TPF\park <parkjonghyeok2000@gmail.com>
2023-05-25 15:53:00 +0900

    Online TechTalk 준비 자료

---
+++ Park/스터디 준비자료/테코톡 자료/ajaxTest/src/main/java/com/example/ajaxtest/dto/AjaxDto.java
@@ -0,0 +1,15 @@
+package com.example.ajaxtest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class AjaxDto {
+    private String param1;
+    private String param2;
+}
