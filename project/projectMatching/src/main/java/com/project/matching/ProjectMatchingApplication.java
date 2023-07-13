5884bcd89f0b65265f53f81ebecb67a40e1f6143
ppark__jjong <101304928+Parkjonghyeok2@users.noreply.github.com>
2023-07-13 15:33:31 +0900

    project Base Commit

---
+++ project/projectMatching/src/main/java/com/project/matching/ProjectMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.project.matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMatchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMatchingApplication.class, args);
+    }
+
+}
